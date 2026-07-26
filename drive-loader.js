a400760626144878a97ba6c94b32ac51ed1d5060
JoyceGeorge23 <joycegeorgesamy23@gmail.com>
2026-07-26 12:05:03 +0300

    fix: polyfill DOMMatrix for pdf-parse

--- drive-loader.js
+++ drive-loader.js
@@ -5,6 +5,15 @@ require("dotenv").config();
 const { google } = require("googleapis");
 const path = require("path");
 const fs = require("fs");
+
+// Polyfill DOMMatrix for pdf-parse in Node.js (Vercel Serverless environment)
+if (typeof global.DOMMatrix === 'undefined') {
+  global.DOMMatrix = class DOMMatrix {
+    constructor() {
+      this.a = 1; this.b = 0; this.c = 0; this.d = 1; this.e = 0; this.f = 0;
+    }
+  };
+}
 const pdfParse = require("pdf-parse");
 const mammoth = require("mammoth");
 
